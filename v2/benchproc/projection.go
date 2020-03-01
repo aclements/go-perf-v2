c68ff5b399717d96178ac8add2e6b42f93db52ed
Austin Clements <austin@google.com>
2020-02-29 22:08:14 -0500

    v2/benchproc: tidy Projection APIs
    Change-Id: Ia37f00fd131f5a2ac7e46ab32e2fb33267cc67ff

--- v2/benchproc/projection.go
+++ v2/benchproc/projection.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// XXX The Projector is not turning out to be useful, and is really
-// annoying. Move back to uncached projections.
-
 package benchproc
 
 import (
@@ -14,60 +11,20 @@ import (
 // A Projection extracts some aspect of a benchmark result into a
 // Config.
 type Projection interface {
-	Project(*Projector) *Config
-}
-
-// A Projector applies Projections to a benchmark result. This stores
-// the state necessary to apply a Projection and caches their results.
-type Projector struct {
-	configSet *ConfigSet
-	result    *benchfmt.Result
-	cache     map[Projection]projCache
-	gen       uint64
-}
-
-type projCache struct {
-	gen uint64
-	val *Config
-}
-
-func NewProjector(cs *ConfigSet) *Projector {
-	return &Projector{cs, nil, make(map[Projection]projCache), 0}
-}
-
-func (p *Projector) Reset(res *benchfmt.Result) {
-	p.gen++
-	p.result = res
-}
-
-func (p *Projector) Project(projection Projection) *Config {
-	if cached, ok := p.cache[projection]; ok && cached.gen == p.gen {
-		return cached.val
-	}
-	val := projection.Project(p)
-	p.cache[projection] = projCache{p.gen, val}
-	return val
-}
-
-func (p *Projector) ConfigSet() *ConfigSet {
-	return p.configSet
-}
-
-func (p *Projector) Cur() *benchfmt.Result {
-	return p.result
+	Project(*ConfigSet, *benchfmt.Result) *Config
 }
 
 // A ProjectProduct combines the results of one or more other
 // projections into a tuple.
 type ProjectProduct []Projection
 
-func (p *ProjectProduct) Project(pr *Projector) *Config {
+func (p *ProjectProduct) Project(cs *ConfigSet, r *benchfmt.Result) *Config {
 	// Invoke each child projection.
 	subs := make([]*Config, 0, 16)
 	for _, proj := range *p {
-		subs = append(subs, pr.Project(proj))
+		subs = append(subs, proj.Project(cs, r))
 	}
-	return pr.ConfigSet().Tuple(subs...)
+	return cs.Tuple(subs...)
 }
 
 // ProjectFileKey projects a file configuration key.
@@ -78,14 +35,13 @@ type ProjectFileKey struct {
 // Project returns a key/value Config with the key p.Key and a value
 // of the file configuration key p.Key, or "" if the key is not
 // present.
-func (p *ProjectFileKey) Project(pr *Projector) *Config {
-	res := pr.Cur()
-	pos, ok := res.FileConfigIndex(p.Key)
+func (p *ProjectFileKey) Project(cs *ConfigSet, r *benchfmt.Result) *Config {
+	pos, ok := r.FileConfigIndex(p.Key)
 	val := ""
 	if ok {
-		val = res.FileConfig[pos].Value
+		val = r.FileConfig[pos].Value
 	}
-	return pr.ConfigSet().KeyVal(p.Key, val)
+	return cs.KeyVal(p.Key, val)
 }
 
 // TODO: If any name keys are extracted, perhaps ProjectFullName needs
@@ -97,9 +53,8 @@ type ProjectFullName struct{}
 
 // Project returns a key/value Config with the key ".name" and a value
 // of the full name of the benchmark.
-func (p *ProjectFullName) Project(pr *Projector) *Config {
-	cs := pr.ConfigSet()
-	return cs.KeyVal(".name", cs.Bytes(pr.Cur().FullName))
+func (p *ProjectFullName) Project(cs *ConfigSet, r *benchfmt.Result) *Config {
+	return cs.KeyVal(".name", cs.Bytes(r.FullName))
 }
 
 // ProjectFileConfig projects the full file configuration as a tuple
@@ -110,7 +65,7 @@ type ProjectFileConfig struct {
 	order map[string]int
 }
 
-func (p *ProjectFileConfig) Project(pr *Projector) *Config {
+func (p *ProjectFileConfig) Project(cs *ConfigSet, r *benchfmt.Result) *Config {
 	// TODO: Collect keys in consistent order. If the file config
 	// was a map, I could just iterate over the order and collect
 	// keys and check if I got all of them and, if not, iterate
